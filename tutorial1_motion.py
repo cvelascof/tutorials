602bc53d75de63bc48e042628535cfcdb3bd61af
ned <daniele.nerini@meteoswiss.ch>
2018-06-18 20:18:00 +0200

    tutorial 1 up to motion field estimation

--- tutorial1_motion.py
+++ tutorial1_motion.py
@@ -2,12 +2,6 @@
 
 The tutorial guides you into the basic notions and techniques for extrapolation 
 nowcasting. 
-
-There are two possible ways to run the tutorials:
-    1. You make the python script executable ("chmod + x tutorial1_motion.py") 
-        and run it (./tutorial1_motion.py).
-    2. You open a python session and copy the script step by step so that you 
-        have more time to understand how the code works.
 """
 
 from __future__ import division
@@ -22,122 +16,175 @@ sys.path.append('../') # add root pySTEPS dir to system path
 
 from iotools import archive, importers, utils
 from motion import optflow, advection
-from datatools.conversion import mmhr2dBR, dBR2mmhr, dBZ2mmhr
+from datatools import conversion, dimension
 from stepsnwc.nowcast_generators import simple_advection
 from visualization.precipfields import plot_precip_field
+from visualization.motionfields import plot_motion_field_quiver, plot_motion_field_streamplot
 
 # List of case studies that can be used in this tutorial
 
 #+-------+--------------+-------------+----------------------------+
 #| event |  start_time  | data_source | description                |
 #+=======+==============+=============+============================+
-#|   1   | 201701121200 |     mch     | orographic precipitation   |
+#|   1   | 201701311000 |     mch     | orographic precipitation   |
 #+-------+--------------+-------------+----------------------------+
-#|   2   | 201701121200 |     mch     | orographic precipitation   |
+#|   2   | 201505151600 |     mch     | non-stationary field       |
 #+-------+--------------+-------------+----------------------------+
 
 # Set parameters for this tutorial
 
 ## input data (copy/paste values from table above)
-start_time     = "201701121200"
-data_source    = "mch"
+startdate_str   = "201701311000"
+data_source     = "mch"
 
 ## data paths
-path_inputs  = "/scratch/ned/tmp/tutorial_data/in"
-path_outputs = "/scratch/ned/tmp/tutorial_data/out"
+path_inputs     = "/scratch/ned/tmp/tutorial_data/in"
+path_outputs    = "/scratch/ned/tmp/tutorial_data/out"
 
-if path_inputs is None or path_outputs is None:
-    raise ValueError("Please define paths to data folders")
+## methods
+oflow_method    = "lucaskanade"
+adv_method      = "semilagrangian"
 
 ## forecast parameters
-n_lead_times = 12
+n_lead_times    = 12
+R_threshold     = 0.1 # [mmhr]
+
+## optical flow parameters
+oflow_kwargs    = {
+                # to control the smoothness of the motion field
+                "decl_grid"         : 3,
+                "min_nr_samples"    : 1,
+                "kernel_bandwidth"  : None
+                }
+
+## visualization parameters
+colorscale      = 'STEPS-BE'
+motion_plot     = 'streamplot' # streamplot or quiver
  
 # Read-in the data
+print('Read the data...')
 
 ## data specifications
 if data_source=="fmi":
-    fn_pattern = "%Y%m%d%H%M_fmi.radar.composite.lowest_FIN_SUOMI1"
-    fn_ext     = "pgm.gz"
-    time_step_min = 5 # timestep between two radar images
+    fn_pattern      = "%Y%m%d%H%M_fmi.radar.composite.lowest_FIN_SUOMI1"
+    fn_ext          = "pgm.gz"
+    time_step_min   = 5 # timestep between two radar images
+    data_units      = 'dBZ'
+    importer        = importers.read_pgm
+    importer_kwargs = {"gzipped":True}
 
 if data_source=="mch":
-    fn_pattern = "AQC%y%j%H%M?_00005.801"
-    fn_ext     = "gif"
-    time_step_min = 5
+    fn_pattern      = "AQC%y%j%H%M?_00005.801"
+    fn_ext          = "gif"
+    time_step_min   = 5
+    data_units      = 'mmhr'
+    importer        = importers.read_aqc
+    importer_kwargs = {}
+    
+startdate  = datetime.datetime.strptime(startdate_str, "%Y%m%d%H%M")
 
-## read-in previous rainfall fields
-startdate  = datetime.datetime.strptime(start_time, "%Y%m%d%H%M")
-input_files = archive.find_by_date(startdate, path_inputs, "", fn_pattern, fn_ext, time_step_min, 3)
+## find radar field filenames
+input_files = archive.find_by_date(startdate, path_inputs, "", fn_pattern, fn_ext, 
+                                   time_step_min, 2)
 if all(fpath is None for fpath in input_files[0]):
     raise ValueError("Input data not found")
 
-R, geodata, metadata = utils.read_timeseries(input_files_prev, importers.read_pgm, gzipped=True)
-if data_source=="fmi":
-    R = dBZ2mmhr(dBZ, R_threshold=0.1)
-    print(input_files_prev[1])
-
-# Read-in future rainfall fields (for verification)
-enddate = startdate + datetime.timedelta(minutes=time_step_min + time_step_min*n_next_images)
-input_files_next = archive.find_by_date(enddate, path_inputs, "", fn_pattern, fn_ext, time_step_min, n_next_images-1)
-dBZ_obs,_,_ = utils.read_timeseries(input_files_next, importers.read_pgm, gzipped=True)
-
-R_obs = dBZ2mmhr(dBZ_obs, R_threshold=0.1)
-
-# Check the structure of the data
+## read radar field files
+R, geodata, metadata = utils.read_timeseries(input_files, importer, **importer_kwargs)
 print("Size of data array (n_times, n_rows, n_cols):", R.shape)
-n_rows = R.shape[1]
-n_cols = R.shape[2]
-field_dim = [n_rows, n_cols]
+orig_field_dim = R.shape
 
-# Read-in a single file to get the geodata
-xmin = geodata['x1']
-xmax = geodata['x2']
-ymin = geodata['y1']
-ymax = geodata['y2']
-extent = np.array([xmin, xmax, ymin, ymax])/1000
+# Prepare input files
+print('Prepare the data...')
 
-###### PLOT THE RADAR RAINFALL FIELDS
-# Plot the sequence of radar fields
-plt.figure()
-for i in range(0, R.shape[0]):
-    R_field = R[i,:,:]
-    time = input_files_prev[1][i]
-    
-    # Plot fig
-    plt.clf()
-    plot_precip_field(R_field, units='mmhr', colorscale='STEPS-BE', extent=extent, title=time, colorbar=True)
-    
-    plt.pause(0.1)
-    plt.draw()
-    
-    # Save fig
-    time_str = time.strftime('%Y%m%d%H%M')
-    figName = path_figs + '/radar_obs_' + time_str + '.png'
-    plt.savefig(figName)
-    print('Saved: ', figName)
+## convert units
+if data_units is 'dBZ':
+    R = conversion.dBZ2mmhr(R, R_threshold)
 
-# Convert linear rainrates to logarithimc dBR units
-R_threshold = 0.1
-dBR, dBRmin = mmhr2dBR(R, R_threshold)
+## make sure we work with a square domain
+R_ = dimension.square_domain(R, 'crop')
+
+## visualize the input radar fields
+print('Plot the data...')
+doanimation     = False
+savefig         = False
+loop = 0
+nloops = 5
+while loop < nloops:
+    
+    for i in xrange(R.shape[0]):
+        plt.clf()
+        if doanimation:
+            plot_precip_field(R[i,:,:], geodata, units='mmhr', colorscale=colorscale, 
+                              title=input_files[1][i], colorbar=True)
+                    
+            plt.pause(.5)
+        
+        if loop==0 and savefig:
+            time_str = input_files[1][i].strftime('%Y%m%d%H%M')
+            figName = path_outputs + '/radar_obs_' + time_str + '.png'
+            plt.savefig(figName)
+            print('Saved: ', figName)
+    
+    if doanimation:
+        plt.pause(1)
+    loop += 1
+    
+# YOUR TURN:
+# Is the radar animation OK? Do the data look correct and in the right order?
+# If yes, then delete the command below to continue this tutorial.
+
+sys.exit()
+
+## convert linear rainrates to logarithimc dBR units
+dBR, dBRmin = conversion.mmhr2dBR(R_, R_threshold)
 dBR[~np.isfinite(dBR)] = dBRmin
 
-###### COMPUTE MOTION FIELD
+# Compute motion field
 print('Computing motion vectors...')
-oflow_method = optflow.get_method("lucaskanade") # Choose optical flow method to define a function handle
-n_prev_images = 2 # Number of previous images to use (minimum = 2)
-V = oflow_method(dBR[-n_prev_images:, :, :]) # Use the function handle to start the motion estimation
-print('done.')
 
-## Plot motion field
-# plt.figure()
-# R_field = R[0,:,:]
-# plot_precip_field(R_field, units='mmhr', colorscale='STEPS-BE', colorbar=True)
-# plot_motion_field_quiver(V, step=100)
-# plt.show()
+oflow_method = optflow.get_method(oflow_method)
+UV = oflow_method(dBR, **oflow_kwargs) 
+
+print('Plot the motion field...')
+doanimation     = True
+savefig         = True
+loop = 0
+nloops = 5
+while loop < nloops:
+    
+    for i in xrange(R.shape[0]):
+        plt.clf()
+        if doanimation:
+            plot_precip_field(R_[i,:,:], None, units='mmhr', colorscale=colorscale, 
+                              title='Motion field', colorbar=True)
+            if motion_plot=='quiver':
+                plot_motion_field_quiver(UV, None, 20)
+            if motion_plot=='streamplot':    
+                plot_motion_field_streamplot(UV, None)        
+            plt.pause(.5)
+        
+        if loop==0 and savefig:
+            time_str = input_files[1][i].strftime('%Y%m%d%H%M')
+            figName = path_outputs + '/radar_motionfield_' + time_str + '.png'
+            plt.savefig(figName)
+            print('Saved: ', figName)
+    
+    if doanimation:
+        plt.pause(1)
+    loop += 1
+
+# YOUR TURN:
+# Try to modify some of the optical flow parameters to see what changes in the
+# estimation of the motion field. 
+# Do the precipitation patterns move along the estimated motion field?
+# If yes, then delete the command below to continue this tutorial.
+
+sys.exit()
 
 ###### EXTRAPOLATE LAST FIELD BASED ON MOTION
 print('Computing extrapolation...')
-adv_method = advection.get_method("semilagrangian") # Choose advection method
+adv_method = advection.get_method(adv_method) 
 dBR_forecast = adv_method(dBR[-1,:,:], V, n_next_images) # Extrapolate the last radar image for a certain number of time steps
 print('done.')
 
